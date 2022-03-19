9d99c24b83dd3dc1badc76a86f3a9c35fc1c148f
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-03-19 18:34:21 +0200

    Class start

--- react-class-03-16/src/index.js
+++ react-class-03-16/src/index.js
@@ -2,12 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, long: null, errMsg: "" };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        }),
+      (err) => {
+        this.setState({ errMsg: err.message });
+      }
     );
-    return <div>Latitude: </div>;
+  }
+
+  render() {
+    if (this.state.errMsg && !this.state.lat && !this.state.long) {
+      return <div>Error: {this.state.errMsg}</div>;
+    }
+    if (!this.state.errMsg && this.state.lat && this.state.long) {
+      return (
+        <div>
+          Latitude: {this.state.lat}
+          <br />
+          Longtitude: {this.state.long}
+        </div>
+      );
+    }
+    return <div>Loading</div>
   }
 }
 
