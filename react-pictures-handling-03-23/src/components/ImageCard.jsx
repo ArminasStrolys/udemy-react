51363d69dba2505b1f4d1f1162524c0190c4d5ef
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-03-27 22:33:24 +0300

    Pic search app - done

--- react-pictures-handling-03-23/src/components/ImageCard.jsx
+++ react-pictures-handling-03-23/src/components/ImageCard.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 export default class imageCard extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { spans: 0 }
+    this.imageRef = React.createRef()
+  }
+
+componentDidMount(){
+  this.imageRef.current.addEventListener('load', this.setSpans)
+}
+
+setSpans = () => {
+const height = this.imageRef.current.clientHeight
+const spans = Math.ceil(height / 20)
+this.setState({ spans })
+}
+
   render() {
+    const { description, urls } = this.props.image
     return (
-      <div>
-      <img src={this.props.image.urls.regular} alt={this.props.image.description} />
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+      <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     )
   }
